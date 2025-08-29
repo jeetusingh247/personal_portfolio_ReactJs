016cecfbd46f4adf2ec6a3f991e0163a8f94c50d
jeetusingh247 <jeetusingh11010@gmail.com>
2025-08-29 17:06:07 +0530

    modified the layout, size

--- jeetu-singh/src/components/Button.jsx
+++ jeetu-singh/src/components/Button.jsx
@@ -9,39 +9,66 @@ import PropTypes from 'prop-types'
 
 // --> Primary Button
 
-const ButtonPrimary = ({
-    href,
-    target = '_self',
-    label,
-    icon,
-    classes
-}) => {
-    if (href) {
-        return (
-            <a href={href} target={target} className={"btn btn-primary " + classes}>
-                {label}
-                {icon ?
-                <span className='material-symbols-rounded' aria-hidden='true' >
-                    {icon}
-                </span>
-                : undefined 
-            }
-            </a>
-        )
-    } else {
-        return (
-            <button className={"btn btn-primary " + classes}>
-                {label}
+import React, { useRef } from 'react';
 
-                {icon ?
-                <span className='material-symbols-rounded' aria-hidden='true' >
-                    {icon}
-                </span>
-                : undefined 
-            }
-            </button>
-        )
-    }
+const useMagnetic = () => {
+    const ref = useRef(null);
+    React.useEffect(() => {
+        const el = ref.current;
+        if (!el) return;
+        const handleMouseMove = e => {
+            const rect = el.getBoundingClientRect();
+            const x = e.clientX - rect.left - rect.width / 2;
+            const y = e.clientY - rect.top - rect.height / 2;
+            el.style.transform = `translate(${x * 0.12}px, ${y * 0.12}px)`;
+        };
+        const handleMouseLeave = () => {
+            el.style.transform = '';
+        };
+        el.addEventListener('mousemove', handleMouseMove);
+        el.addEventListener('mouseleave', handleMouseLeave);
+        return () => {
+            el.removeEventListener('mousemove', handleMouseMove);
+            el.removeEventListener('mouseleave', handleMouseLeave);
+        };
+    }, []);
+    return ref;
+};
+
+const ButtonPrimary = ({
+        href,
+        target = '_self',
+        label,
+        icon,
+        classes
+}) => {
+        const magneticRef = useMagnetic();
+        if (href) {
+                return (
+                        <a href={href} target={target} className={"btn btn-primary " + classes} ref={magneticRef}>
+                                {label}
+                                {icon ?
+                                <span className='material-symbols-rounded' aria-hidden='true' >
+                                        {icon}
+                                </span>
+                                : undefined 
+                        }
+                        </a>
+                )
+        } else {
+                return (
+                        <button className={"btn btn-primary " + classes} ref={magneticRef}>
+                                {label}
+
+                                {icon ?
+                                <span className='material-symbols-rounded' aria-hidden='true' >
+                                        {icon}
+                                </span>
+                                : undefined 
+                        }
+                        </button>
+                )
+        }
 }
 
 ButtonPrimary.propTypes = {
