6209be9c58244317d51e4df89002dbd719a1f4c1
jeetusingh247 <jeetusingh11010@gmail.com>
2025-08-29 17:09:57 +0530

    modified

--- jeetu-singh/src/App.jsx
+++ jeetu-singh/src/App.jsx
@@ -3,6 +3,15 @@
 * @license Apache-2.0
 */
 
+// --> Node Modules
+import {ReactLenis} from 'lenis/react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useGSAP }  from '@gsap/react';
+
+// --> Regsiter GSAP Plugins
+gsap.registerPlugin(useGSAP, ScrollTrigger);
+
 // --> Components
 import Header from './components/header';
 import Hero from './components/Hero';
@@ -11,10 +20,28 @@ import Skill from './components/Skill';
 import Work from './components/Work';
 import Review from './components/Review';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 const App = () => {
+
+    useGSAP(() => {
+        const elements = gsap.utils.toArray('.reveal-up');
+        elements.forEach((element) => {
+            gsap.to(element, {
+                scrollTrigger: {
+                    trigger: element,
+                    scrub: true,
+                },
+                y: 0,
+                opacity: 1,
+                duration: 1,
+                ease: 'power2.out'
+            });
+        });
+    });
+
     return (
-        <>
+        <ReactLenis root>
         <Header />
         <main>
             <Hero />
@@ -24,7 +51,8 @@ const App = () => {
             <Review />
             <Contact />
         </main>
-        </>
+        <Footer />
+        </ReactLenis>
     )
 }
 
